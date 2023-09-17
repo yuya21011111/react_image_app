8db9790c8c63d377c8c434e5ba978b988bc874ea
YuyaYamazi <kaimomo12345@gmail.com>
2023-09-17 16:28:48 +0900

    API準備

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import RandomArray from './components/RandomArray';
 import ImageGrallery from './components/ImageGarllery';
+import { useState } from 'react';
+import { useRef } from 'react';
 
 function App() {
+  const [image, setImage] = useState('')
+  const ref = useRef();
+  const changeImage = e => {
+    e.preventDefault()
+    console.log(ref.current.value)
+
+    const searchUrl = 'https://pixabay.com/api/?key=39492364-927383ed74e45fc24e58f0f68'
+  }
+  
+ 
   return (
     <div className="container">
       <h2>My images</h2>
-      <form>
-          <input type='text' placeholder="画像をさがそう!" />
-      </form>
+          <input type='text' onChange={e => changeImage(e)} placeholder="画像をさがそう!" ref={ref} />
       <ImageGrallery />
     </div>
   );
