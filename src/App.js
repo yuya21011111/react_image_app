ff56da9b76b90bd1948d4639ebbcdd85a2c7fd47
YuyaYamazi <kaimomo12345@gmail.com>
2023-09-18 16:30:10 +0900

    API制作処理

--- src/App.js
+++ src/App.js
@@ -6,13 +6,25 @@ import { useState } from 'react';
 import { useRef } from 'react';
 
 function App() {
-  const [image, setImage] = useState('')
+  const [imageData, setImage] = useState([])
   const ref = useRef();
   const changeImage = e => {
     e.preventDefault()
     console.log(ref.current.value)
 
-    const searchUrl = 'https://pixabay.com/api/?key=39492364-927383ed74e45fc24e58f0f68'
+    const searchUrl = 'https://pixabay.com/api/?key=39492364-927383ed74e45fc24e58f0f68&'
+    const params = { q: ref.current.value, image_type: 'photo', }
+    const queryParams = new URLSearchParams(params)
+    const jsonUrl = searchUrl + queryParams
+    
+    fetch(jsonUrl)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data.hits)
+        setImage(data.hits)
+      })
   }
   
  
@@ -20,7 +32,7 @@ function App() {
     <div className="container">
       <h2>My images</h2>
           <input type='text' onChange={e => changeImage(e)} placeholder="画像をさがそう!" ref={ref} />
-      <ImageGrallery />
+      <ImageGrallery imageData={imageData} />
     </div>
   );
 }
--- src/components/ImageGarllery.js
+++ src/components/ImageGarllery.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import '../ImagesGrallery.css'
 import { useState } from 'react'
-const ImageGrallery = () => {
+const ImageGrallery = ({ imageData }) => {
    
     return(<>
       <div className="images-wrapper">
-        <div className='image'>
-            <img src='https://media.istockphoto.com/id/1131336837/ja/%E3%82%B9%E3%83%88%E3%83%83%E3%82%AF%E3%83%95%E3%82%A9%E3%83%88/%E9%A3%9F%E7%89%A9%E3%82%92%E6%8C%81%E3%81%A4%E7%99%BD%E3%81%84%E3%83%8E%E3%83%89%E3%81%AE%E7%8E%8B%E6%A7%98%E3%83%95%E3%82%A3%E3%83%83%E3%82%B7%E3%83%A3%E3%83%BC.jpg?s=1024x1024&w=is&k=20&c=iNiGCmX3191zkTRjPeFynAjeGMs9oQdnO51YLvowzo4=' alt=''
-            />
-        </div>
+        {imageData.map((image) => (
+             <div className='image'　key={image.id}>
+            <a href={image.pageURL} target='"_blank'>
+              <img src={image.largeImageURL} alt=''/>
+            </a>
+         </div>
+        ))}
       </div>
     </>)
 }
